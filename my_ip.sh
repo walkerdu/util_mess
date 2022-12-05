c674ba9e2e5ceb2ed4d644d47c4355c7d295bca3
walkerdu <walkerdu@qq.com>
2022-12-05 16:15:47 +0800

    add my ip and region info script

---
+++ my_ip.sh
@@ -0,0 +1,15 @@
+echo '----------------------------'
+echo 'curl cip.cc'
+echo '----------------------------'
+curl cip.cc
+echo '----------------------------'
+
+echo 'curl ipinfo.io	# US'
+echo '----------------------------'
+curl ipinfo.io
+echo '----------------------------'
+
+echo 'curl myip.ipip.net'
+echo '----------------------------'
+curl myip.ipip.net
+echo '----------------------------'
